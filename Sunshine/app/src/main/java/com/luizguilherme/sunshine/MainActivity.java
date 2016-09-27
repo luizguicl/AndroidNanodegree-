cd499e72c4aae041a2d0ed0b551677dab849417e
Luiz Guilherme <luizguicl@gmail.com>
2016-09-26 21:49:48 -0300

    Connecting sunshine to the clould

--- Sunshine/app/src/main/java/com/luizguilherme/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/luizguilherme/sunshine/MainActivity.java
@@ -1,17 +1,7 @@
 package com.luizguilherme.sunshine;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,45 +11,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public static class PlaceholderFragment extends Fragment {
-
-        private ArrayAdapter<String> forecastAdapter;
-
-        public PlaceholderFragment() {
-
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] data = {
-                    "Mon 6/23 - Sunny - 31/17",
-                    "Tue 6/24 - Foggy - 21/8",
-                    "Wed 6/25 - Cloudy - 22/17",
-                    "Thurs 6/26 - Rainy - 18/11",
-                    "Fri 6/27 - Foggy - 21/10",
-                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                    "Sun 6/29 - Sunny - 20/7"
-            };
-
-            List<String> weekForecast = Arrays.asList(data);
-
-            forecastAdapter =
-                    new ArrayAdapter<>(
-                            getActivity(),
-                            R.layout.list_item_forecast,
-                            R.id.list_item_forecast_textview,
-                            weekForecast);
-
-
-            ListView forecastListView = (ListView) rootView.findViewById(R.id.list_view_forecast);
-
-            forecastListView.setAdapter(forecastAdapter);
-
-            return rootView;
-        }
-    }
 }
